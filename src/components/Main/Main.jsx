0f7b1d1524801ddea01d202c209a797600b7bf57
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-11-29 17:29:52 -0800

    connecting files and importing data

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css'
+import ZodiacCard from '../ZodiacCard/ZodiacCard'
+import { zodiac } from '../../data'
+
+export default function Main() {
+  return (
+    <main>
+      <ZodiacCard />
+    </main>
+  )
+}
---
+++ src/components/ZodiacCard/ZodiacCard.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './ZodiacCard.css'
+
+export default function ZodiacCard() {
+  return <div>This is a Zodiac Card.</div>
+}
